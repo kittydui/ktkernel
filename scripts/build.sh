445bf4859fcad0b187906e2f9ae3076b1e202652
dui <bruhrain@riseup.net>
2026-03-29 17:07:14 +0100

    Cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,10 @@ mkdir -p build
 
 (
     cd build
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=../Toolchain.cmake ..
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain.cmake ..
     cmake --build .
 )
 
 ln -sf ./build/compile_commands.json ./compile_commands.json
 
-./make_root.sh
+./scripts/make_root.sh
